b6179161a59ae21762d84874284505f38e3fef73
shubhamavantkar <avantkarshubham99@gmail.com>
2025-01-31 06:48:42 +0530

    fix: search box in project dashboard

--- lib/widgets/searchField.dart
+++ lib/widgets/searchField.dart
@@ -5,22 +5,26 @@ class SearchField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SearchBar(
-      leading: Image.asset(
-        'assets/search.png',
-        height: 20,
+    return SizedBox(
+      width: 374,
+      height: 44,
+      child: SearchBar(
+        leading: Image.asset(
+          'assets/search.png',
+          height: 20,
+        ),
+        hintText: 'Search Project',
+        hintStyle: WidgetStatePropertyAll(TextStyle(
+            color: Colors.grey.withOpacity(0.5),
+            fontSize: 14,
+            fontWeight: FontWeight.w600)),
+        elevation: WidgetStatePropertyAll(0),
+        backgroundColor: WidgetStatePropertyAll(Colors.white),
+        shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+            side: const BorderSide(
+                color: Color(0xFFD5E0F6), style: BorderStyle.solid))),
       ),
-      hintText: 'Search Project',
-      hintStyle: WidgetStatePropertyAll(TextStyle(
-          color: Colors.grey.withValues(alpha: 0.5),
-          fontSize: 14,
-          fontWeight: FontWeight.w600)),
-      elevation: WidgetStatePropertyAll(0),
-      backgroundColor: WidgetStatePropertyAll(Colors.white),
-      shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-          side:
-              BorderSide(color: Color(0xFFD5E0F6), style: BorderStyle.solid))),
     );
   }
 }
