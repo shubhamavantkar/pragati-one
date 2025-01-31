6633143bef49bbb5f3fee2b03cb885e9c80e5b04
shubhamavantkar <avantkarshubham99@gmail.com>
2025-01-31 05:31:54 +0530

    modified: Add project

--- lib/controllers/authController.dart
+++ lib/controllers/authController.dart
@@ -73,6 +73,8 @@ class AuthController {
     return false;
   }
 }
-
-
 }
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:pragati/constants/consts.dart';
 import 'package:pragati/pages/landingPage.dart';
+import 'package:pragati/pages/dashboardScreen.dart'; // Import the ProjectDashboard
 
-void main() {
-  runApp(const MyApp());
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure that async code runs before app starts
+  bool isLoggedIn = await checkIfLoggedIn(); // Check if token exists
+
+  runApp(MyApp(isLoggedIn: isLoggedIn)); // Pass the login status to the MyApp
+}
+
+Future<bool> checkIfLoggedIn() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? token = prefs.getString('authToken'); // Retrieve the token
+  print('Token: $token');
+  return token != null && token.isNotEmpty; // If token exists, return true
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLoggedIn;
 
-  // This widget is the root of your application.
+  const MyApp({super.key, required this.isLoggedIn});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pragati',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
-
         fontFamily: 'Metropolis',
-        // textTheme: TextTheme(headlineLarge: ),
         colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
         useMaterial3: true,
       ),
-      home: LandingPage(),
+      home: isLoggedIn ? DashboardScreen() : LandingPage(), // Navigate based on login status
     );
   }
 }
